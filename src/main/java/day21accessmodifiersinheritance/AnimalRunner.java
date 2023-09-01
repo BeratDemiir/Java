499f5830ca9cef94128a9ab3dea4b964b9ced98c
Berat Demir <brtdmr2726@gmail.com>
2023-09-01 15:56:38 +0300

    01.09.23

--- src/main/java/day21accessmodifiersinheritance/AnimalRunner.java
+++ src/main/java/day21accessmodifiersinheritance/AnimalRunner.java
@@ -2,6 +2,26 @@ package day21accessmodifiersinheritance;
 
 public class AnimalRunner {
 
+    /*
+       1) "Inheritance" sayesinde
+            i) Code tekrarlarindan kurtuluruz
+            ii) Code tamiri(maintenance) kolay olur
+            iii) Child Class'lari dha atomic yapmis oluruz
+
+       2) Bir Class'i baska bir Class'in Child Class'i yapmak icin
+          "Extends" keyword kullanilir. ılk yazilan Class Child, ikinci
+          yazilan Class parent olur
+
+       3) child Class object'leri Parent Class'dan method ve variable'leri
+          kullanabilirler
+
+       4) Parent Class object'leri Child Class'dan method ve variable'leri
+          kullanamazlar
+
+       5) Object Class her class'in parent'idir
+          Java da Object Class haric her Class'in parent'i vardir.
+     */
+
     public static void main(String[] args) {
 
         Cat c1 = new Cat();
