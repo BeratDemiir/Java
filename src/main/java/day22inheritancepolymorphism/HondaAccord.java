716219d286ba641f1fb8eb95ff409067ab13f063
Berat Demir <brtdmr2726@gmail.com>
2023-09-04 18:47:09 +0300

    04.09.23

--- src/main/java/day22inheritancepolymorphism/HondaAccord.java
+++ src/main/java/day22inheritancepolymorphism/HondaAccord.java
@@ -8,7 +8,11 @@ public class HondaAccord extends Honda{
         constructor'in sart oldugunu bilir.
         2) Java'nin otamatik olarak olusturdugu bu gorunmez constructor'a "default constructor" denir.
         3) Siz kendiniz herhangi bir constructor olusturdugunuzda Java "default constructor" i siler.
-        4) Bir Class'da birden fazla constructor olabilir. Fakat bu constructor'larin parametre'leri
+        4) Bir Class'da birden fazla constructor olabilir. Fakat bu constructor'larin parametre'leri farkli olmalidir.
+        5) "this" keyword "Bu Class" anlamindadir. "this.price" demek bu Class'daki "price" isimli variable demektir.
+           "this.price" syntax'i constructor'larin icinde kullanir.
+        6) Constructor kullanarak variable'lar uzerinde yaptiginiz degisimler sadece Object uzerindeki variable'larin degerlerini
+           degistirir. Class daki variable degerlerini degistiremez...
      */
 
     public int price;
@@ -26,6 +30,7 @@ public class HondaAccord extends Honda{
     public HondaAccord(int price,int year){
         this.price =price;
         this.year =year;
+        System.out.println("Honda Accord Constructor");
     }
 
     public HondaAccord(int price, int year, String make, String model){
---
+++ src/main/java/day22inheritancepolymorphism/VehicleRunner.java
@@ -0,0 +1,11 @@
+package day22inheritancepolymorphism;
+
+public class VehicleRunner {
+
+    public static void main(String[] args) {
+
+        // Example: Car turunde rengi beyaz olan 54 bin km 250 bin tl fiyati olan 2020 model bir honda Accort ariyorum.
+
+        HondaAccord hacc = new HondaAccord(250000,2020);
+    }
+}
