7c2da49f396f0392dfa54328f8467249e59d37dd
Berat Demir <brtdmr2726@gmail.com>
2023-09-05 23:07:39 +0300

    05.09.23

--- src/main/java/day24statickeyword_encapsulation/CarRunner.java
+++ src/main/java/day24statickeyword_encapsulation/CarRunner.java
@@ -3,13 +3,24 @@ package day24statickeyword_encapsulation;
 public class CarRunner {
 
 
+    /*
+        Static Keyword nedir?
+         1) Class'a baglanmis Class elamanidir.
+         2) Static Class elamanlari butun Objelerin ortak elamanidir.
+         3) Static uzerinde yapilan her degisiklikte butun Objeleri etkiler butun Objeler tarafindan gorulur.
+         4) Static Class elamanlarina Class uzerinden ulasilir. Objeler static'lere ulasmak icin kullanilmaz(tavsiye edilmez)
+         5) Static'ler Overriding edilemez, cunku ortak method oldugu icin herkes etkilenir ve izin verilmez.
+     */
     public static void main(String[] args) {
         Car car1 = new Car();
         Car car2 = new Car();
         Car car3 = new Car();
         Car car4 = new Car();
 
+        // static variable'lara objeleri kullanarak ulasmak mumkun ama tavsiye edilmez.
         System.out.println(car1.counter);// 4
+        // static variable'lara Class ismi kullanarak ulasilmalidir
+        System.out.println(Car.counter);
         System.out.println(car1.price);// 20001
 
 
