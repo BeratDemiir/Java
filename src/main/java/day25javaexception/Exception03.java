e673c02dff962a39890059e315536fd99d018771
Berat Demir <brtdmr2726@gmail.com>
2023-09-06 22:55:13 +0300

    06.09.23

--- src/main/java/day25javaexception/Exception03.java
+++ src/main/java/day25javaexception/Exception03.java
@@ -4,6 +4,27 @@ public class Exception03 {
 
     public static void main(String[] args) {
 
+        String str = "Ali";
+        getNumOfChars(str);// 3
 
+        String s = "";
+        getNumOfChars(s);// 0
+
+        String t = null;
+        getNumOfChars(t);// NullPointerException
+        // Eger length() methodunda null kullanirsaniz bu (NullPointerException) exception'i alirsiniz
+
+    }
+
+    // Bir String'de bulunan charecterlerin sayisini bulabilmek icin bir method olusturun.
+
+    public static void getNumOfChars(String str){
+
+        try {
+            System.out.println(str.length());
+        }catch (NullPointerException e){
+            System.err.println("length() methodunda bir problem olustu "+ e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
