7d12ee5d0f5a650a084670b607e0ee1e66ddaea2
Berat Demir <brtdmr2726@gmail.com>
2023-09-18 18:56:34 +0300

    18.09.23

---
+++ src/main/java/day01variables/C00_Variable01.java
@@ -0,0 +1,22 @@
+
+    package day01variables;
+
+    public class C00_Variable01 {
+
+        public static void main(String[] args) {
+
+            int sayi=20;
+
+            System.out.println(sayi); // 20 sayi variable'nin degerini yazdirir
+
+            System.out.println("sayi"); // sayi
+
+            sayi=30;
+            System.out.println(sayi); // 30
+
+            sayi= sayi + 20;
+
+            System.out.println(sayi); // 50
+        }
+    }
+
