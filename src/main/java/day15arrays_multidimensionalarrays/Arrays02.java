9328aef6000ce9bdf2ad4ee1769aea12e323164c
Berat Demir <brtdmr2726@gmail.com>
2024-01-21 13:22:18 +0300

    21.01.24

--- src/main/java/day15arrays_multidimensionalarrays/Arrays02.java
+++ src/main/java/day15arrays_multidimensionalarrays/Arrays02.java
@@ -7,10 +7,8 @@ public class Arrays02 {
     public static void main(String[] args) {
 
         // Example 1: Size verilen pozitif ve negatif sayilar iceren bir integer array'deki "en buyuk negatif" ve "en kucuk pozitif" elamani bulunuz.
-
         int arr[] ={-12, 18, -5, 23, -2, 0};
         Arrays.sort(arr);
-
         int maxNegative =arr[0];
         int minPozitive =arr[arr.length-1];
 
